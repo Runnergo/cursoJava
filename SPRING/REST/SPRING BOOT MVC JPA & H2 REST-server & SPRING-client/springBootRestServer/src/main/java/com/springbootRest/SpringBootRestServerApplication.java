e8a651c70eb2fef59725c3e3285bea2927db65a0
Runnergo <51993715+Runnergo@users.noreply.github.com>
2019-10-30 21:10:34 +0100

    Add files via upload

---
+++ SPRING/REST/SPRING BOOT MVC JPA & H2 REST-server & SPRING-client/springBootRestServer/src/main/java/com/springbootRest/SpringBootRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.springbootRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServerApplication.class, args);
+	}
+
+}
---
+++ SPRING/REST/SPRING BOOT MVC JPA & H2 REST-server & SPRING-client/springBootRestServer/src/test/java/com/springBootRest/SpringBootRestServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springBootRest;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
